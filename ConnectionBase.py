f5573600d2f97493007250cfb23b8cbc7c9ea532
Marcin Jóźwikowski <marcin@jozwikowski.pl>
2017-08-04 20:51:03 +0200

    IP list for Windows

--- ConnectionBase.py
+++ ConnectionBase.py
@@ -1,8 +1,6 @@
 import platform
 import socket
-
 import subprocess
-
 import re
 
 
@@ -16,7 +14,6 @@ class ConnectionBase(object):
     connection = None
     file = None
     file_chunks_total = 0
-
 
     comm_end_trans = b'END'
     comm_file_header = b'FILE'
@@ -38,6 +35,31 @@ class ConnectionBase(object):
     def set_status_callback(self, callback):
         self.statusCallback = callback
 
+    def get_all_ips(self):
+        sys_name = platform.system()
+        if sys_name == 'Windows':
+            return self.get_windows_ips()
+        raise Exception('System not supported: ' + sys_name)
+
+    def get_windows_ips(self):
+        ips = {}
+        proc = subprocess.Popen(['ipconfig'], stdout=subprocess.PIPE)
+        name_regex = re.compile('^([A-Za-z0-9 \-_])+:')
+        ip_regex = re.compile('^[\s]+IPv4[\w\s.]+:[\s]*(([\d]{0,3}.){4})')
+        section_name = ""
+        while True:
+            line = proc.stdout.readline().decode("utf-8")
+            if line != '':
+                name_search = name_regex.search(line)
+                if name_search:
+                    section_name = name_search.group()
+                ip_search = ip_regex.search(line)
+                if ip_search:
+                    ip_value = ip_search.group(1).strip()
+                    ips[ip_value] = ip_value + " - " + section_name[:-1]
+            else:
+                break
+        return ips
 
 if __name__ == '__main__':
     print("Class not callable")
